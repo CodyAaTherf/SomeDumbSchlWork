f0d0e01a9dd2c3f041c32de893f6f18ea38004c3
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-01-26 19:45:54 +0530

    memes inc || avg temp
    Meme: How I do vs how the World does.

---
+++ Schl/temp.py
@@ -0,0 +1,24 @@
+"""
+THIS IS HOW THE WORLD DOES
+"""
+
+d1_temp = int(input("Enter the temperature of 1st Day: "))
+d2_temp = int(input("Enter the temperature of 2nd Day: "))
+d3_temp = int(input("Enter the temperature of 3rd Day: "))
+avg_temp = (d1_temp + d2_temp + d3_temp) / 3
+print("Average temperature of 3 days is {0}".format(avg_temp))
+
+"""
+THIS IS HOW I DO
+"""
+
+# import statistics
+
+# d1_temp = int(input("Enter the temperature of 1st Day: "))
+# d2_temp = int(input("Enter the temperature of 2nd Day: "))
+# d3_temp = int(input("Enter the temperature of 3rd Day: "))
+
+# temp = [d1_temp , d2_temp , d3_temp]
+# avg = statistics.mean(temp)
+
+# print("Average temperature is {0}".format(avg))

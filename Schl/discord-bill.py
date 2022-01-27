ad70136973bfbbc6469eb2667d3f8975e8d84838
Altersplatz <78807470+Altersplatz@users.noreply.github.com>
2022-01-27 11:32:06 +0530

    Refactored Code

--- Schl/discord-bill.py
+++ Schl/discord-bill.py
@@ -1,4 +1,4 @@
-bill_amt = float(input("Bill Amount: "))
+# Refactored
 
 def calc_dis(bill_amt):
     if bill_amt > 2000:
@@ -9,7 +9,9 @@ def calc_dis(bill_amt):
         return  bill_amt*(1)
     return bill_amt
 
-print(f"Total bill amount = {calc_dis(bill_amt)}")
+if __name__ == "__main__":
+    bill_amt = float(input("Bill Amount: "))
+    print(f"Total bill amount = {calc_dis(bill_amt)}")
 
 """
 I HAVE MADE THIS ONE A BIT SIMPLE. I COULD HAVE USED `if __name__ == "__main__":`
